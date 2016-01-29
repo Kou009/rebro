71adc404a6ea95f0f0e7ba5e9f3526ffb2e61318
Kou Ihara <Kou@MacBook-Air-5.local>
2016-01-29 11:31:28 +0800

    ichiran,indexの変更です

--- Theme/assets/js/login.js
+++ Theme/assets/js/login.js
@@ -162,4 +162,26 @@ $('#contact-form').bootstrapValidator({
         }
     });
 
+ /**index,html*/
+
+ $(document).ready(function () {
+    $('#myCarousel').carousel({
+        interval: 10000
+    })
+    $('.fdi-Carousel .item').each(function () {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        if (next.next().length > 0) {
+            next.next().children(':first-child').clone().appendTo($(this));
+        }
+        else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+    });
+});
+
     
